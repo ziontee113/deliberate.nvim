41c6f1563453b681173952316488c2f96f69ab61
ziontee113 <ziontee113@gmail.com>
2023-05-16 04:49:30 +0700

    chore: format helpers module

--- lua/nvim-stormcaller/helpers/init.lua
+++ lua/nvim-stormcaller/helpers/init.lua
@@ -1,9 +1,7 @@
 local M = {}
 
 M.set_buf_content = function(content)
-    if type(content) == "string" then
-        content = vim.split(content, "\n")
-    end
+    if type(content) == "string" then content = vim.split(content, "\n") end
     vim.api.nvim_buf_set_lines(0, 0, -1, false, content)
 end
 
