9787428902ecb5b899c48dff2b196c3f8145562e
ziontee113 <ziontee113@gmail.com>
2023-05-16 21:09:36 +0700

    chore(helper): extend test buffer content

--- lua/nvim-stormcaller/helpers/init.lua
+++ lua/nvim-stormcaller/helpers/init.lua
@@ -66,6 +66,59 @@ function OtherOtherComponent() {
       </ul>
     </div>
   )
+}
+
+import Image from 'next/image'
+import { useState } from 'react'
+
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function Gallery({ images }: { images: Image[] }) {
+  return (
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image}></BlurImage>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true)
+
+  return (
+    <a href={image.imageSrc} className="group">
+      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          fill
+          style={{ objectFit: 'cover' }}
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+    </a>
+  )
 }]])
 end
 
