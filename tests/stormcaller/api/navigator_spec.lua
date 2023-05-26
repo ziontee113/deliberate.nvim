3079cea627a5b73093757ac0161a5c928761e689
ziontee113 <ziontee113@gmail.com>
2023-05-27 04:14:47 +0700

    ref(navi): continue to refactor tests

--- tests/stormcaller/api/navigator_spec.lua
+++ tests/stormcaller/api/navigator_spec.lua
@@ -5,10 +5,11 @@ local selection = require("stormcaller.lib.selection")
 local navigator = require("stormcaller.api.navigator")
 local helpers = require("stormcaller.helpers")
 
-local initiate = function(cmd, wanted_text)
+local initiate = function(cmd, wanted_text, assert_fn)
     vim.cmd(string.format("norm! %s", cmd))
     catalyst.initiate({ win = 0, buf = 0 })
-    helpers.assert_catalyst_node_has_text(wanted_text)
+    assert_fn = assert_fn or helpers.assert_catalyst_node_has_text
+    assert_fn(wanted_text)
 end
 
 local move = function(destination, wanted_text, position, assert_fn)
@@ -18,20 +19,18 @@ local move = function(destination, wanted_text, position, assert_fn)
     assert.are.same(position, vim.api.nvim_win_get_cursor(0))
 end
 
+local long_li_tag = [[<li>
+          A new study found that coffee drinkers have a lower risk of liver
+          cancer. So, drink up!
+        </li>]]
+
 describe("typescriptreact navigator.move()", function()
     before_each(function() helpers.set_buffer_content_as_multiple_react_components() end)
     after_each(function() helpers.clean_up() end)
 
     it("direction = next-sibling", function()
         initiate("17gg^", "<li>Home</li>")
-        move(
-            "next-sibling",
-            [[<li>
-          A new study found that coffee drinkers have a lower risk of liver
-          cancer. So, drink up!
-        </li>]],
-            { 18, 8 }
-        )
+        move("next-sibling", long_li_tag, { 18, 8 })
         move("next-sibling", "<li>Contacts</li>", { 22, 8 })
         move("next-sibling", "<li>FAQ</li>", { 23, 8 })
         move("next-sibling", "<OtherComponent />", { 24, 8 })
@@ -41,163 +40,53 @@ describe("typescriptreact navigator.move()", function()
     it("direction = previous-sibling", function()
         initiate("23gg^", "<li>FAQ</li>")
         move("previous-sibling", "<li>Contacts</li>", { 22, 8 })
-        move( -- put cursor at end of tag since we're moving up and target start and end not on same line.
-            "previous-sibling",
-            [[<li>
-          A new study found that coffee drinkers have a lower risk of liver
-          cancer. So, drink up!
-        </li>]],
-            { 21, 12 }
-        )
+        move("previous-sibling", long_li_tag, { 21, 12 }) -- put cursor at end of tag since we're moving up and target start and end not on same line
         move("previous-sibling", "<li>Home</li>", { 17, 8 })
         move("previous-sibling", "<li>Home</li>", { 17, 8 }) -- stays in place since no more prevous sibling
     end)
 
     it("direction = next", function()
-        vim.cmd("norm! 16gg^") -- cursor to start of <div> tag
-
-        catalyst.initiate({ win = 0, buf = 0 })
-        helpers.assert_first_line_of_catalyst_node_has_text(
-            '<div className="h-screen w-screen bg-zinc-900">'
+        initiate(
+            "16gg",
+            '<div className="h-screen w-screen bg-zinc-900">',
+            helpers.assert_first_line_of_catalyst_node_has_text
         )
 
-        -- 1st move
-        navigator.move({ destination = "next" })
-        helpers.assert_catalyst_node_has_text("<li>Home</li>")
-
-        local cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 17, 8 }, cursor_positon)
-
-        -- 2nd move
-        navigator.move({ destination = "next" })
-        helpers.assert_catalyst_node_has_text([[<li>
-          A new study found that coffee drinkers have a lower risk of liver
-          cancer. So, drink up!
-        </li>]])
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 18, 8 }, cursor_positon)
-
-        -- 3rd move
-        navigator.move({ destination = "next" })
-        helpers.assert_catalyst_node_has_text("<li>Contacts</li>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 22, 8 }, cursor_positon)
-
-        -- 5th move
-        navigator.move({ destination = "next" })
-        navigator.move({ destination = "next" })
-
-        helpers.assert_catalyst_node_has_text("<OtherComponent />")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 24, 8 }, cursor_positon)
-
-        -- 6th move
-        navigator.move({ destination = "next" })
-        helpers.assert_last_line_of_catalyst_node_has_text("      </div>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 25, 11 }, cursor_positon)
-
-        -- 7th move
-        navigator.move({ destination = "next" })
-        helpers.assert_last_line_of_catalyst_node_has_text("    </>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 26, 6 }, cursor_positon)
-
-        -- 8th move --> should jump to next html Component
-        navigator.move({ destination = "next" })
-        helpers.assert_first_line_of_catalyst_node_has_text("<div>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 34, 4 }, cursor_positon)
-
-        -- 9th move
-        navigator.move({ destination = "next" })
-        helpers.assert_first_line_of_catalyst_node_has_text("<ul>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 35, 6 }, cursor_positon)
+        move("next", "<li>Home</li>", { 17, 8 })
+        move("next", long_li_tag, { 18, 8 })
+        move("next", "<li>Contacts</li>", { 22, 8 })
+        move("next", "<li>FAQ</li>", { 23, 8 })
+        move("next", "<OtherComponent />", { 24, 8 })
+        move("next", "      </div>", { 25, 11 }, helpers.assert_last_line_of_catalyst_node_has_text)
+        move("next", "    </>", { 26, 6 }, helpers.assert_last_line_of_catalyst_node_has_text)
+        move("next", "<div>", { 34, 4 }, helpers.assert_first_line_of_catalyst_node_has_text) --> should jump to next html Component
+        move("next", "<ul>", { 35, 6 }, helpers.assert_first_line_of_catalyst_node_has_text)
     end)
 
     it("direction = previous", function()
-        vim.cmd("norm! 22gg^") -- cursor to start 3rd <li> tag
-
-        catalyst.initiate({ win = 0, buf = 0 })
-        helpers.assert_catalyst_node_has_text("<li>Contacts</li>")
-
-        -- 1st move
-        navigator.move({ destination = "previous" })
-        helpers.assert_catalyst_node_has_text([[<li>
-          A new study found that coffee drinkers have a lower risk of liver
-          cancer. So, drink up!
-        </li>]])
-
-        local cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 21, 12 }, cursor_positon)
-
-        -- 2nd move
-        navigator.move({ destination = "previous" })
-        helpers.assert_catalyst_node_has_text("<li>Home</li>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 17, 8 }, cursor_positon)
-
-        -- 3rd move
-        navigator.move({ destination = "previous" })
-        helpers.assert_first_line_of_catalyst_node_has_text(
-            [[<div className="h-screen w-screen bg-zinc-900">]]
+        initiate("22gg^", "<li>Contacts</li>")
+        move("previous", long_li_tag, { 21, 12 })
+        move("previous", "<li>Home</li>", { 17, 8 })
+        move(
+            "previous",
+            '<div className="h-screen w-screen bg-zinc-900">',
+            { 16, 6 },
+            helpers.assert_first_line_of_catalyst_node_has_text
         )
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 16, 6 }, cursor_positon)
-
-        -- 4th move
-        navigator.move({ destination = "previous" })
-        helpers.assert_first_line_of_catalyst_node_has_text("<>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 15, 4 }, cursor_positon)
-
-        -- 5th move
-        navigator.move({ destination = "previous" })
-        helpers.assert_last_line_of_catalyst_node_has_text("    </p>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 6, 7 }, cursor_positon)
+        move("previous", "<>", { 15, 4 }, helpers.assert_first_line_of_catalyst_node_has_text)
+        move("previous", "    </p>", { 6, 7 }, helpers.assert_last_line_of_catalyst_node_has_text)
     end)
 
     it("direction = parent", function()
-        vim.cmd("norm! 22gg^") -- cursor to start 3rd <li> tag
-
-        catalyst.initiate({ win = 0, buf = 0 })
-        helpers.assert_catalyst_node_has_text("<li>Contacts</li>")
-
-        -- 1st move
-        navigator.move({ destination = "parent" })
-        helpers.assert_first_line_of_catalyst_node_has_text(
-            [[<div className="h-screen w-screen bg-zinc-900">]]
+        initiate("22gg^", "<li>Contacts</li>")
+        move(
+            "parent",
+            '<div className="h-screen w-screen bg-zinc-900">',
+            { 16, 6 },
+            helpers.assert_first_line_of_catalyst_node_has_text
         )
-
-        local cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 16, 6 }, cursor_positon)
-
-        -- 2nd move
-        navigator.move({ destination = "parent" })
-        helpers.assert_first_line_of_catalyst_node_has_text("<>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 15, 4 }, cursor_positon)
-
-        -- 3rd move, should stand still since no more html parent from here
-        navigator.move({ destination = "parent" })
-        helpers.assert_first_line_of_catalyst_node_has_text("<>")
-
-        cursor_positon = vim.api.nvim_win_get_cursor(0)
-        assert.are.same({ 15, 4 }, cursor_positon)
+        move("parent", "<>", { 15, 4 }, helpers.assert_first_line_of_catalyst_node_has_text)
+        move("parent", "<>", { 15, 4 }, helpers.assert_first_line_of_catalyst_node_has_text) -- stands still since no more html parent from here
     end)
 end)
 
