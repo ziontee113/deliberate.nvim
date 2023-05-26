b1b6e72aabf92138f34d04dacd671c0b12903452
ziontee113 <ziontee113@gmail.com>
2023-05-26 08:24:22 +0700

    feat(svelte): add get_tag_identifier_node()

--- lua/stormcaller/lib/tree-sitter/svelte.lua
+++ lua/stormcaller/lib/tree-sitter/svelte.lua
@@ -14,4 +14,16 @@ M.get_all_html_nodes_in_buffer = function(buf)
     return all_html_nodes, grouped_captures
 end
 
+M.get_tag_identifier_node = function(node)
+    local first_child = lib_ts.get_children_with_types({
+        node = node,
+        desired_types = { "start_tag", "self_closing_tag" },
+    })[1]
+    if not first_child then error("can't find correct child element") end
+    return lib_ts.get_children_with_types({
+        node = first_child,
+        desired_types = { "tag_name" },
+    })[1]
+end
+
 return M
--- tests/stormcaller/lib/tree-sitter/svelte_spec.lua
+++ tests/stormcaller/lib/tree-sitter/svelte_spec.lua
@@ -14,3 +14,27 @@ describe("get_all_html_nodes_in_buffer()", function()
         assert.equals(17, #grouped_captures["element"])
     end)
 end)
+
+describe("get_tag_identifier_node()", function()
+    helpers.set_buffer_content_as_svelte_file()
+
+    it("works for element that has both starting and closing elements", function()
+        local all_nodes = lib_ts_svelte.get_all_html_nodes_in_buffer(0)
+        local normal_node = all_nodes[15]
+        helpers.assert_node_has_text(normal_node, "<h1>Ligma</h1>")
+
+        local tag_idenifier_node = lib_ts_svelte.get_tag_identifier_node(normal_node)
+        helpers.assert_node_has_text(tag_idenifier_node, "h1")
+    end)
+
+    it("works for element that has only self_closing_tag", function()
+        local all_nodes = lib_ts_svelte.get_all_html_nodes_in_buffer(0)
+        local self_closing_node = all_nodes[13]
+        helpers.assert_node_has_text(self_closing_node, "<Counter />")
+
+        local tag_idenifier_node = lib_ts_svelte.get_tag_identifier_node(self_closing_node)
+        helpers.assert_node_has_text(tag_idenifier_node, "Counter")
+    end)
+
+    vim.api.nvim_buf_delete(0, { force = true })
+end)
