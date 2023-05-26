fd5791bb82adde769e1459e56f638a213722fc8e
ziontee113 <ziontee113@gmail.com>
2023-05-27 03:00:40 +0700

    ref(aggregator): add error handling in invoke()

--- lua/stormcaller/lib/tree-sitter/language_aggregator.lua
+++ lua/stormcaller/lib/tree-sitter/language_aggregator.lua
@@ -10,20 +10,38 @@ local m = {
 
 local ft = function()
     local catalyst = require("stormcaller.lib.catalyst")
-    if not catalyst.buf() then return end
+    if not catalyst.buf() then
+        error("aggregator module was called when catalyst is not initiated")
+    end
     return vim.api.nvim_buf_get_option(catalyst.buf(), "ft")
 end
 
-M.get_all_html_nodes_in_buffer = function(...) return m[ft()].get_all_html_nodes_in_buffer(...) end
-M.get_tag_identifier_node = function(...) return m[ft()].get_tag_identifier_node(...) end
-M.get_className_property_string_node = function(...)
-    return m[ft()].get_className_property_string_node(...)
+local invoke = function(method_name, ...)
+    local filetype = ft()
+    local module = m[filetype]
+    if not module then error(filetype .. "filetype not supported") end
+    if not module[method_name] then
+        error(
+            string.format(
+                'responsible module for %s filetype did not implement "%s()" method',
+                filetype,
+                method_name
+            )
+        )
+    end
+    return module[method_name](...)
 end
-M.extract_class_names = function(...) return m[ft()].extract_class_names(...) end
-M.get_updated_root = function(...) return m[ft()].get_updated_root(...) end
-M.get_html_node = function(...) return m[ft()].get_html_node(...) end
-M.get_first_closing_bracket = function(...) return m[ft()].get_first_closing_bracket(...) end
-M.get_html_children = function(...) return m[ft()].get_html_children(...) end
-M.get_html_siblings = function(...) return m[ft()].get_html_siblings(...) end
+
+M.get_all_html_nodes_in_buffer = function(...) return invoke("get_all_html_nodes_in_buffer", ...) end
+M.get_tag_identifier_node = function(...) return invoke("get_tag_identifier_node", ...) end
+M.get_className_property_string_node = function(...)
+    return invoke("get_className_property_string_node", ...)
+end
+M.extract_class_names = function(...) return invoke("extract_class_names", ...) end
+M.get_updated_root = function(...) return invoke("get_updated_root", ...) end
+M.get_html_node = function(...) return invoke("get_html_node", ...) end
+M.get_first_closing_bracket = function(...) return invoke("get_first_closing_bracket", ...) end
+M.get_html_children = function(...) return invoke("get_html_children", ...) end
+M.get_html_siblings = function(...) return invoke("get_html_siblings", ...) end
 
 return M
