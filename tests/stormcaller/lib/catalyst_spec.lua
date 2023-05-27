5abec7392ed6b037df59b3995d184331052ec3bc
ziontee113 <ziontee113@gmail.com>
2023-05-27 08:32:54 +0700

    ref(catalyst): refactor svelte test cases

--- tests/stormcaller/lib/catalyst_spec.lua
+++ tests/stormcaller/lib/catalyst_spec.lua
@@ -1,7 +1,5 @@
 require("tests.editor_config")
 
-local catalyst = require("stormcaller.lib.catalyst")
-
 local h = require("stormcaller.helpers")
 local initiate = h.initiate_and_check_cursor_positon
 
@@ -37,41 +35,38 @@ describe("catalyst.initiate() for svelte()", function()
     before_each(function() h.set_buffer_content_as_svelte_file() end)
     after_each(function() h.clean_up() end)
 
-    it("puts cursor at start of closest tag, initial cursor inside html_element", function()
-        vim.cmd("norm! 32ggfm")
-        catalyst.initiate({ win = 0, buf = 0 })
-
-        h.catalyst_has("<h1>Ligma</h1>")
-        assert.same({ 32, 4 }, vim.api.nvim_win_get_cursor(0))
-    end)
-
+    it(
+        "puts cursor at start of closest tag, initial cursor inside html_element",
+        function() initiate("32ggfm", "<h1>Ligma</h1>", h.catalyst_has, { 32, 4 }) end
+    )
     it(
         "puts cursor at start of closest tag, initial cursor outside and above html_element",
         function()
-            vim.cmd("norm! 30gg")
-            catalyst.initiate({ win = 0, buf = 0 })
-
-            h.catalyst_has([[<section>
+            initiate(
+                "30gg",
+                [[<section>
     <h1>Ligma</h1>
     <h3>is a made-up term</h3>
     <p>that gained popularity as part of an Internet prank or meme.</p>
-</section>]])
-            assert.same({ 31, 0 }, vim.api.nvim_win_get_cursor(0))
+</section>]],
+                h.catalyst_has,
+                { 31, 0 }
+            )
         end
     )
-
     it(
         "puts cursor at end of closest tag, with initial cursor outside and below html_element",
         function()
-            vim.cmd("norm! 43gg")
-            catalyst.initiate({ win = 0, buf = 0 })
-
-            h.catalyst_has([[<section>
+            initiate(
+                "43gg",
+                [[<section>
     <h1>Ligma</h1>
     <h3>is a made-up term</h3>
     <p>that gained popularity as part of an Internet prank or meme.</p>
-</section>]])
-            assert.same({ 35, 9 }, vim.api.nvim_win_get_cursor(0))
+</section>]],
+                h.catalyst_has,
+                { 35, 9 }
+            )
         end
     )
 end)
