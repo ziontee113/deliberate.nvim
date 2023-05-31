3bdbdbbb7e12dc0273ff1585ea47d00ec95fea48
ziontee113 <ziontee113@gmail.com>
2023-05-31 15:49:37 +0700

    fix(visual): use correct functions for toggle()

--- lua/stormcaller/api/visual_mode.lua
+++ lua/stormcaller/api/visual_mode.lua
@@ -2,11 +2,17 @@ local M = {}
 
 local visual_mode_active = false
 
-M.toogle = function() visual_mode_active = not visual_mode_active end
 M.on = function() visual_mode_active = true end
 M.off = function()
     visual_mode_active = false
     require("stormcaller.lib.selection").update()
+end
+M.toogle = function()
+    if visual_mode_active then
+        M.off()
+    else
+        M.on()
+    end
 end
 
 M.is_active = function() return visual_mode_active end
