fb65d0699602b4e53dfae058d77025b17009fae3
ziontee113 <ziontee113@gmail.com>
2023-06-01 10:42:17 +0700

    feat(Hydra): add pms mappings

--- lua/stormcaller/hydra.lua
+++ lua/stormcaller/hydra.lua
@@ -3,8 +3,90 @@ local catalyst = require("stormcaller.lib.catalyst")
 local visual_collector = require("stormcaller.api.visual_collector")
 local selection = require("stormcaller.lib.selection")
 local navigator = require("stormcaller.api.navigator")
+local pms_menu = require("stormcaller.ui.pms_menu")
 
 local exit_hydra = function() vim.api.nvim_input("<Plug>DeliberateExitHydra") end
+local heads = {
+    {
+        "j",
+        function() navigator.move({ destination = "next" }) end,
+        { nowait = true, desc = "Navigator Move Down" },
+    },
+    {
+        "k",
+        function() navigator.move({ destination = "previous" }) end,
+        { nowait = true, desc = "Navigator Move Up" },
+    },
+
+    {
+        "<Tab>",
+        function() navigator.move({ destination = "next", select_move = true }) end,
+        { nowait = true },
+    },
+    {
+        "<S-Tab>",
+        function() navigator.move({ destination = "previous", select_move = true }) end,
+        { nowait = true },
+    },
+    {
+        "<A-Tab>",
+        function() catalyst.move_to(true) end,
+        { nowait = true },
+    },
+
+    {
+        "v",
+        function() visual_collector.toggle() end,
+        { nowait = true },
+    },
+
+    {
+        "<Esc>",
+        function()
+            if not selection.select_move_is_active() and not visual_collector.is_active() then
+                exit_hydra()
+            end
+            if visual_collector.is_active() then visual_collector.stop() end
+            if selection.select_move_is_active() then selection.clear(true) end
+        end,
+        { nowait = true },
+    },
+
+    -- workaround to programmatically exit Hydra
+    { "<Plug>DeliberateExitHydra", nil, { exit = true } },
+}
+
+--------------------------------------------
+
+local properties = {
+    p = { "", "x", "y", "t", "b", "l", "r" },
+    m = { "", "x", "y", "t", "b", "l", "r" },
+    s = { "x", "y" },
+}
+
+for property, axies in pairs(properties) do
+    for _, axis in ipairs(axies) do
+        local keymap = property .. axis
+        if axis == "" then keymap = string.upper(property) end
+
+        local hydra_mapping = {
+            keymap,
+            function()
+                if property == "p" then
+                    pms_menu.change_padding({ axis = axis })
+                elseif property == "m" then
+                    pms_menu.change_margin({ axis = axis })
+                elseif property == "s" then
+                    pms_menu.change_spacing({ axis = axis })
+                end
+            end,
+            { nowait = true },
+        }
+        table.insert(heads, hydra_mapping)
+    end
+end
+
+--------------------------------------------
 
 Hydra({
     name = "Deliberate",
@@ -22,53 +104,5 @@ Hydra({
     },
     mode = "n",
     body = "<Esc>",
-    heads = {
-        {
-            "j",
-            function() navigator.move({ destination = "next" }) end,
-            { nowait = true },
-        },
-        {
-            "k",
-            function() navigator.move({ destination = "previous" }) end,
-            { nowait = true },
-        },
-
-        {
-            "<Tab>",
-            function() navigator.move({ destination = "next", select_move = true }) end,
-            { nowait = true },
-        },
-        {
-            "<S-Tab>",
-            function() navigator.move({ destination = "previous", select_move = true }) end,
-            { nowait = true },
-        },
-        {
-            "<A-Tab>",
-            function() catalyst.move_to(true) end,
-            { nowait = true },
-        },
-
-        {
-            "v",
-            function() visual_collector.toggle() end,
-            { nowait = true },
-        },
-
-        {
-            "<Esc>",
-            function()
-                if not selection.select_move_is_active() and not visual_collector.is_active() then
-                    exit_hydra()
-                end
-                if visual_collector.is_active() then visual_collector.stop() end
-                if selection.select_move_is_active() then selection.clear(true) end
-            end,
-            { nowait = true },
-        },
-
-        -- workaround to programmatically exit Hydra
-        { "<Plug>DeliberateExitHydra", nil, { exit = true } },
-    },
+    heads = heads,
 })
