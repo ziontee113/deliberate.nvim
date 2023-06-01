b1a7e757ab16bc69a20f3229192e6bf1a54b2202
ziontee113 <ziontee113@gmail.com>
2023-06-01 07:58:41 +0700

    chore: rewrite visual_collector_spec

--- tests/stormcaller/api/visual_collector_spec.lua
+++ tests/stormcaller/api/visual_collector_spec.lua
@@ -1,92 +1,54 @@
 local visual_collector = require("stormcaller.api.visual_collector")
-local navigator = require("stormcaller.api.navigator")
 local selection = require("stormcaller.lib.selection")
 local helpers = require("stormcaller.helpers")
+local mova = helpers.move_then_assert_selection
 
 describe("visual_collector", function()
     helpers.set_buffer_content_as_multiple_react_components()
 
-    it("starts collecting nodes on move with visual_collector.on()", function()
+    it("starts collecting nodes on move after visual_collector.start()", function()
         helpers.initiate("22gg^", "<li>Contacts</li>")
 
         visual_collector.start()
+        helpers.selection_is(1, { "<li>Contacts</li>" })
 
-        navigator.move({ destination = "next" })
+        mova("next", 2, { "<li>Contacts</li>", "<li>FAQ</li>" }, "<li>FAQ</li>")
 
-        assert.equals(2, #selection.nodes())
-        helpers.node_has_text(selection.nodes()[1], "<li>Contacts</li>")
-        helpers.node_has_text(selection.nodes()[2], "<li>FAQ</li>")
-
-        navigator.move({ destination = "next" })
-
-        helpers.selection_is(3, {
+        mova("next", 3, {
             "<li>Contacts</li>",
             "<li>FAQ</li>",
             "<OtherComponent />",
-        })
+        }, "<OtherComponent />")
     end)
 
     it("stops collecting nodes on move with visual_collector.on()", function()
         visual_collector.stop()
-
         helpers.selection_is(3, {
             "<li>Contacts</li>",
             "<li>FAQ</li>",
             "<OtherComponent />",
         })
+    end)
 
+    it("selection only gets cleared if `selection.clear()` is called", function()
         selection.clear()
+        helpers.selection_is(1, "<OtherComponent />")
 
-        navigator.move({ destination = "previous" })
-        assert.equals(1, #selection.nodes())
-        helpers.node_has_text(selection.nodes()[1], "<li>FAQ</li>")
+        mova("previous", 1, "<li>FAQ</li>", "<li>FAQ</li>")
     end)
 
     it("navigator.move({select_move = true}) works as normal", function()
-        navigator.move({ destination = "previous", select_move = true })
-
-        assert.equals(1, #selection.nodes())
-        helpers.node_has_text(selection.nodes()[1], "<li>FAQ</li>")
-
-        navigator.move({ destination = "previous", select_move = true })
-
-        assert.equals(2, #selection.nodes())
-        helpers.node_has_text(selection.nodes()[1], "<li>FAQ</li>")
-        helpers.node_has_text(selection.nodes()[2], "<li>Contacts</li>")
+        mova({ "previous", true }, 1, { "<li>FAQ</li>" }, "<li>Contacts</li>")
+        mova({ "previous", true }, 2, { "<li>FAQ</li>", "<li>Contacts</li>" }, helpers.long_li_tag)
     end)
 
-    helpers.clean_up()
-end)
+    it("use `visual_collector` alongside `select_move`", function()
+        mova("previous", 2, { "<li>FAQ</li>", "<li>Contacts</li>" }, "<li>Home</li>")
 
-describe("combine `select_move` with `visual_mode`", function()
-    helpers.set_buffer_content_as_multiple_react_components()
-
-    it("works", function()
-        helpers.initiate("23gg^", "<li>FAQ</li>")
-
-        -- select nodes using select_move
-        navigator.move({ destination = "previous", select_move = true })
-        navigator.move({ destination = "previous", select_move = true })
-        helpers.selection_is(2, {
-            "<li>FAQ</li>",
-            "<li>Contacts</li>",
-        })
-
-        -- select nodes with Visual Mode
-        helpers.move("previous", "<li>Home</li>")
-        helpers.selection_is(2, {
-            "<li>FAQ</li>",
-            "<li>Contacts</li>",
-        })
         visual_collector.start()
-        helpers.selection_is(3, {
-            "<li>FAQ</li>",
-            "<li>Contacts</li>",
-            "<li>Home</li>",
-        })
+        helpers.selection_is(3, { "<li>FAQ</li>", "<li>Contacts</li>", "<li>Home</li>" })
 
-        navigator.move({ destination = "previous" })
-        helpers.selection_is(4, {
+        mova("previous", 4, {
             "<li>FAQ</li>",
             "<li>Contacts</li>",
             "<li>Home</li>",
