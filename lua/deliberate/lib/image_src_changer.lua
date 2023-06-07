fd73fe609015b2397929846f5064f6d53889d84d
ziontee113 <ziontee113@gmail.com>
2023-06-07 14:50:19 +0700

    chore: correct functin name in `image_src_changer`

--- lua/deliberate/lib/image_src_changer.lua
+++ lua/deliberate/lib/image_src_changer.lua
@@ -7,7 +7,7 @@ local M = {}
 
 ---@param buf number
 ---@param node TSNode
-local function set_empty_className_property_if_needed(buf, node)
+local function set_empty_src_property_if_needed(buf, node)
     if aggregator.get_src_property_string_node(buf, node) then return end
 
     local tag_node = aggregator.get_tag_identifier_node(node)
@@ -25,7 +25,7 @@ M.replace = function(src)
     require("deliberate.api.dot_repeater").register(M.replace, src)
 
     for i = 1, #selection.nodes() do
-        set_empty_className_property_if_needed(catalyst.buf(), selection.nodes()[i])
+        set_empty_src_property_if_needed(catalyst.buf(), selection.nodes()[i])
 
         local src_string_node =
             aggregator.get_src_property_string_node(catalyst.buf(), selection.nodes()[i])
