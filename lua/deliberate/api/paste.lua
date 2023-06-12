63711dc71adfe76866f7f8e1cd4d3e2b19928a10
ziontee113 <ziontee113@gmail.com>
2023-06-12 15:08:01 +0700

    feat(paste): support destination = "inside"

--- lua/deliberate/api/paste.lua
+++ lua/deliberate/api/paste.lua
@@ -1,17 +1,22 @@
 local catalyst = require("deliberate.lib.catalyst")
 local selection = require("deliberate.lib.selection")
 local yank = require("deliberate.api.yank")
+local aggregator = require("deliberate.lib.tree-sitter.language_aggregator")
 
 ---@param lines string[]
 ---@param target_start_col integer
 ---@return string[]
-local reindent = function(lines, target_start_col)
+local reindent = function(lines, target_start_col, destination)
     local shortest_indent_amount = #string.match(lines[1], "^%s+") or 0
     for _, line in ipairs(lines) do
         local this_line_indent = string.match(line, "^%s+")
         if #this_line_indent < shortest_indent_amount then
             shortest_indent_amount = #this_line_indent
         end
+    end
+
+    if destination == "inside" then
+        shortest_indent_amount = shortest_indent_amount - vim.bo.tabstop
     end
 
     if shortest_indent_amount ~= target_start_col then
@@ -31,14 +36,57 @@ local reindent = function(lines, target_start_col)
     return lines
 end
 
+local join_contents = function()
+    local joined_contents = {}
+    for _, yanked_lines in ipairs(yank.contents()) do
+        for _, line in ipairs(yanked_lines) do
+            table.insert(joined_contents, line)
+        end
+    end
+    return joined_contents
+end
+
+local find_indents = function(buf, node)
+    local start_row = node:range()
+    local first_line = vim.api.nvim_buf_get_lines(buf, start_row, start_row + 1, false)[1]
+    return string.match(first_line, "^%s+") or ""
+end
+
+local spread_the_tag = function(i)
+    local first_closing_bracket =
+        aggregator.get_first_closing_bracket(catalyst.buf(), selection.nodes()[i])
+    local _, _, b_row, b_col = first_closing_bracket:range()
+
+    vim.api.nvim_buf_set_text(
+        catalyst.buf(),
+        b_row,
+        b_col,
+        b_row,
+        b_col,
+        { "", find_indents(catalyst.buf(), selection.nodes()[i]) }
+    )
+
+    local tag_node = aggregator.get_tag_identifier_node(selection.nodes()[i])
+    local _, tag_identifier_start = tag_node:range()
+
+    local row_offset = 1
+    local update_row = b_row + row_offset
+    local update_col = tag_identifier_start - 1 + vim.bo.tabstop
+
+    selection.refresh_tree()
+    selection.update_item(i, update_row, update_col)
+end
+
 ---@class paste_Args
----@field destination "previous" | "next"
+---@field destination "previous" | "next" | "inside"
+---@field paste_inside_destination "after-all-children" | "before-all-children"
 ---@field join boolean | nil
 ---@field reindent boolean
 
 ---@type paste_Args
 local default_paste_opts = {
     destination = "next",
+    paste_inside_destination = "after-all-children",
     join = true,
     reindent = true,
 }
@@ -53,22 +101,51 @@ M.call = function(opts)
     selection.archive_for_undo()
     require("deliberate.api.dot_repeater").register(M.call, opts)
 
-    local joined_contents = {}
-    for _, yanked_lines in ipairs(yank.contents()) do
-        for _, line in ipairs(yanked_lines) do
-            table.insert(joined_contents, line)
-        end
-    end
-
     for i = 1, #selection.nodes() do
-        local lines = opts.join and joined_contents or yank.contents()[i]
+        local lines = opts.join and join_contents() or yank.contents()[i]
+        local mutable_destination = opts.destination
+
+        local target_row, row_offset
+
+        if opts.destination == "inside" then
+            local html_children = aggregator.get_html_children(selection.nodes()[i])
+            if #html_children == 0 then
+                spread_the_tag(i)
+            else
+                if opts.paste_inside_destination == "after-all-children" then
+                    local child_start_row, child_start_col = html_children[#html_children]:range()
+                    selection.refresh_tree()
+                    selection.update_item(i, child_start_row, child_start_col)
+                    mutable_destination = "next"
+                else
+                    local child_start_row, child_start_col = html_children[1]:range()
+                    selection.refresh_tree()
+                    selection.update_item(i, child_start_row, child_start_col)
+                    mutable_destination = "previous"
+                end
+            end
+        end
 
         local start_row, start_col, end_row, _ = selection.nodes()[i]:range()
 
-        if opts.reindent then lines = reindent(lines, start_col) end
+        if opts.reindent then lines = reindent(lines, start_col, mutable_destination) end
 
-        local target_row = opts.destination == "previous" and start_row or end_row
-        local row_offset = opts.destination == "previous" and 0 or 1
+        if opts.destination == "previous" then
+            target_row, row_offset = start_row, 0
+        elseif opts.destination == "next" then
+            target_row, row_offset = end_row, 1
+        elseif
+            opts.destination == "inside"
+            and opts.paste_inside_destination == "after-all-children"
+        then
+            target_row, row_offset = start_row, 1
+        elseif
+            opts.destination == "inside"
+            and opts.paste_inside_destination == "before-all-children"
+        then
+            target_row, row_offset = start_row, 0
+        end
+
         target_row = target_row + row_offset
 
         vim.api.nvim_buf_set_lines(selection.buf(), target_row, target_row, false, lines)
--- tests/deliberate/api/paste_spec.lua
+++ tests/deliberate/api/paste_spec.lua
@@ -5,6 +5,8 @@ local yank = require("deliberate.api.yank")
 local paste = require("deliberate.api.paste")
 local h = require("deliberate.helpers")
 local movA = h.move_then_assert_selection
+local html_tag = require("deliberate.api.html_tag")
+local navigator = require("deliberate.api.navigator")
 
 describe("paste()", function()
     before_each(function() h.set_buffer_content_as_multiple_react_components() end)
@@ -78,4 +80,101 @@ describe("paste()", function()
       </div>]]
         )
     end)
+
+    it(
+        "destination = inside, consecutive multi selection, paste destination does not have html children",
+        function()
+            h.initiate("22gg^", "<li>Contacts</li>")
+            movA({ "next", true }, 1, "<li>Contacts</li>")
+            movA({ "next", true }, 2, { "<li>Contacts</li>", "<li>FAQ</li>" })
+
+            yank.call()
+            h.selection_is(1, "<OtherComponent />") -- selection gets cleared if `yank.call()` with no args
+
+            html_tag.add({ destination = "next", tag = "div", content = "" })
+            h.selection_is(1, "<div></div>")
+
+            paste.call({ destination = "inside" })
+
+            h.node_has_text(
+                selection.nodes()[1]:parent(),
+                [[<div className="h-screen w-screen bg-zinc-900">
+        <li>Home</li>
+        <li>
+          A new study found that coffee drinkers have a lower risk of liver
+          cancer. So, drink up!
+        </li>
+        <li>Contacts</li>
+        <li>FAQ</li>
+        <OtherComponent />
+        <div>
+          <li>Contacts</li>
+          <li>FAQ</li>
+        </div>
+      </div>]]
+            )
+        end
+    )
+
+    it(
+        "destination = inside, consecutive multi selection, paste destination has html children",
+        function()
+            h.initiate("22gg^", "<li>Contacts</li>")
+            movA({ "next", true }, 1, "<li>Contacts</li>")
+            movA({ "next", true }, 2, { "<li>Contacts</li>", "<li>FAQ</li>" })
+
+            yank.call()
+            h.selection_is(1, "<OtherComponent />") -- selection gets cleared if `yank.call()` with no args
+
+            navigator.move({destination = "parent"})
+
+            paste.call({ destination = "inside", paste_inside_destination = "after-all-children" })
+            h.node_has_text(
+                selection.nodes()[1]:parent(),
+                [[<div className="h-screen w-screen bg-zinc-900">
+        <li>Home</li>
+        <li>
+          A new study found that coffee drinkers have a lower risk of liver
+          cancer. So, drink up!
+        </li>
+        <li>Contacts</li>
+        <li>FAQ</li>
+        <OtherComponent />
+        <li>Contacts</li>
+        <li>FAQ</li>
+      </div>]]
+            )
+        end
+    )
+
+    it(
+        "destination = inside, paste before all target's children",
+        function()
+            h.initiate("22gg^", "<li>Contacts</li>")
+            movA({ "next", true }, 1, "<li>Contacts</li>")
+            movA({ "next", true }, 2, { "<li>Contacts</li>", "<li>FAQ</li>" })
+
+            yank.call()
+            h.selection_is(1, "<OtherComponent />") -- selection gets cleared if `yank.call()` with no args
+
+            navigator.move({destination = "parent"})
+
+            paste.call({ destination = "inside", paste_inside_destination = "before-all-children" })
+            h.node_has_text(
+                selection.nodes()[1]:parent(),
+                [[<div className="h-screen w-screen bg-zinc-900">
+        <li>Contacts</li>
+        <li>FAQ</li>
+        <li>Home</li>
+        <li>
+          A new study found that coffee drinkers have a lower risk of liver
+          cancer. So, drink up!
+        </li>
+        <li>Contacts</li>
+        <li>FAQ</li>
+        <OtherComponent />
+      </div>]]
+            )
+        end
+    )
 end)
